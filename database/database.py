e9ba8981c7dd4a5ac9b975b751e38aced720de4b
AIT MHIND YOUSSEF <youssefaitmhend22@gmail.com>
2026-05-02 17:39:11 +0100

    fix: replacing the one connection design, with connection per method call

--- database/database.py
+++ database/database.py
@@ -3,8 +3,10 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.connection = sqlite3.connect("omnipulse.db")
-        cursor = self.connection.cursor()
+
+        self.path = "omnipulse.db"
+        connection = sqlite3.connect(self.path)
+        cursor = connection.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS targets (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,48 +17,42 @@ class Database:
                 selectors TEXT
             );
         """)
-        self.connection.commit()
+        connection.commit()
         cursor.close()
         
-    def commit(self):
-        self.connection.commit()
 
     def create_target(self,url,name,last_seen,title,selectors):
-        print(title)
-        print(selectors)
-        cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO targets(url,name,last_seen,title,selectors) VALUES(?,?,?,?,?)",(url,name,last_seen,title,selectors))
-        cursor.close()
-        self.commit()
-    
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO targets(url,name,last_seen,title,selectors) VALUES(?,?,?,?,?)",(url,name,last_seen,title,selectors))
+            connection.commit()
+        
     def update_target(self,id,selectors):
-        cursor = self.connection.cursor()
-        cursor.execute("UPDATE targets SET selectors = ? WHERE id = ?",(selectors,id))
-        cursor.close()
-        self.commit()
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            cursor.execute("UPDATE targets SET selectors = ? WHERE id = ?",(selectors,id))
+            connection.commit()
     
     def update_state(self,id,last_seen,title):
-        cursor = self.connection.cursor()
-        cursor.execute("UPDATE targets SET last_seen = ? title = ? WHERE id = ?",(last_seen,title,id))
-        cursor.close()
-        self.commit()
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            cursor.execute("UPDATE targets SET last_seen = ? title = ? WHERE id = ?",(last_seen,title,id))
+            connection.commit()
     
     def find_target_by_name(self,name) -> dict | None:
-        cursor = self.connection.cursor()
-        res = cursor.execute("SELECT * FROM targets WHERE name = ?",(name,)).fetchone()
-
-        return None if res is None else {"id": res[0], "url": res[1], "name": res[2], "selectors": res[3]}
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            res = cursor.execute("SELECT * FROM targets WHERE name = ?",(name,)).fetchone()
+            return None if res is None else {"id": res[0], "url": res[1], "name": res[2], "selectors": res[3]}
     
     def find_target_by_url(self,url) -> dict | None:
-        cursor = self.connection.cursor()
-        res = cursor.execute("SELECT * FROM targets WHERE url = ?",(url,)).fetchone()
-
-        return None if res is None else {"id": res[0], "url": res[1], "name": res[2], "selectors": res[3]}
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            res = cursor.execute("SELECT * FROM targets WHERE url = ?",(url,)).fetchone()
+            return None if res is None else {"id": res[0], "url": res[1], "name": res[2], "selectors": res[3]}
     
     def findall(self):
-        cursor = self.connection.cursor()
-        res = cursor.execute("SELECT * FROM targets").fetchall()
-
-        
-
-        return None if res is None else [{"id": r[0], "url": r[1], "name": r[2], "selectors": r[3]} for r in res]
+        with sqlite3.connect(self.path) as connection:
+            cursor = connection.cursor()
+            res = cursor.execute("SELECT * FROM targets").fetchall()
+            return None if res is None else [{"id": r[0], "url": r[1], "name": r[2], "last_seen": r[3], "title": r[4], "selectors": r[5]} for r in res]
