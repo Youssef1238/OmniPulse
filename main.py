64eb8369598106cc3678ad8c35b781df4a0d4225
AIT MHIND YOUSSEF <youssefaitmhend22@gmail.com>
2026-05-02 17:39:50 +0100

    feat: database integration and external urls redirection

--- main.py
+++ main.py
@@ -2,20 +2,55 @@ from core.scraper import Scrape,Compare
 import time
 import random
 import traceback
-from ui.index import start
+import webview
+import os
+from database.database import Database
+from utils.helpers import get_full_link
+
+
+class API:
+    def __init__(self):
+        self.db = Database()
+
+    def route(self,path):
+        window.load_url(path)
+
+    def redirect(self,url):
+        os.startfile(url)
+    
+    def get_link(self,url,path):
+        return get_full_link(path,url)
+
+    def load_data(self):
+        data = self.db.findall()
+        return data
+    def log(self,content):
+        print(content)
+
+
+if __name__ == '__main__':
+    api = API()
+    window = webview.create_window('OmniPulse', url='ui/index.html',js_api=api)
+    webview.start()
+
+
+
+
+
+
 
 #url = input("Type the url: ")
 #url = "https://www.goud.ma/topics/%d8%a7%d9%84%d8%b1%d8%a6%d9%8a%d8%b3%d9%8a%d8%a9/"
 url = "https://www.bladi.net/maroc-sport.html"
 
 
-try:
-    """ while True:
+""" try:
+     while True:
         html_content = Scrape(url)
 
         Compare(url,html_content)
-        time.sleep(60 + random.randint(-5,5)) """
+        time.sleep(60 + random.randint(-5,5)) 
     start()
 
 except Exception as e:
-    traceback.print_exc()
+    traceback.print_exc() """
--- ui/index.js
+++ ui/index.js
@@ -1,21 +1,40 @@
-clickMe = document.getElementById("click-me")
-targets = document.querySelector(".targets ul")
 links = document.querySelectorAll(".link")
 
+dashboard = document.getElementById('dashboard')
 
-clickMe.addEventListener('click',async (e)=>{
-    const res = await pywebview.api.load_data()
-    pywebview.api.log(res)
-    for (r of res){
-        child = document.createElement("li")
-        child.textContent  = r["target"]
-        targets.appendChild(child)
-    }
-})
 
 for(link of links){
     link.addEventListener('click',(e)=>{
-        
         pywebview.api.route(e.target.getAttribute("href"))
     })
 }
+
+
+
+window.addEventListener("pywebviewready",async (e)=>{
+    const data = await pywebview.api.load_data()
+    
+    for(row of data){
+        const articleLink = await pywebview.api.get_link(row["url"],row["last_seen"]) 
+        const item = document.createElement('tr')
+        const name = document.createElement('td')
+        const article = document.createElement('td') 
+        name.setAttribute("href",row["url"])
+        name.setAttribute("class","url")
+        article.setAttribute("class","url")
+        article.setAttribute("href",articleLink)
+        name.innerHTML = row["name"]
+        article.innerHTML = row["title"].length > 50 ? row["title"].slice(0,50) + "..." : row["title"]
+        item.appendChild(name)
+        item.appendChild(article)
+        dashboard.appendChild(item)
+    }
+
+    urls = document.querySelectorAll(".url")
+
+    for(url of urls){
+        url.addEventListener('click',(e)=>{
+            pywebview.api.redirect(e.target.getAttribute("href"))
+        })
+    }
+})
